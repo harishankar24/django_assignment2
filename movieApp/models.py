5b8f625156eb53cd117acb3c6d0dd62017bb03e1
Hari <hari@shankar.com>
2023-03-31 19:14:47 +0530

    Read operation view completed. Next add add-movie link

--- movieApp/models.py
+++ movieApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=60)
+    releaseDate = models.DateField()
+    rating = models.FloatField()
+
+    class Meta:
+        db_table = 'movie'
--- movieApp/views.py
+++ movieApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Movie
+
 
 # Create your views here.
+def movieList(request):
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {'movies':movies})
